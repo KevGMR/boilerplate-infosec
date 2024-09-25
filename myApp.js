9e716bf4a92cf5da21c86bcf569cec7dd4a172fd
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-09-25 22:04:04 +0300

    added the actual middleware

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.use(helmet.ieNoOpen())
 // X-Download-Options header to noopen.
 
 const ninetyDaysInSeconds = 90*24*60*60;
-app.use({maxAge: ninetyDaysInSeconds, force: true})
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 
 
 
